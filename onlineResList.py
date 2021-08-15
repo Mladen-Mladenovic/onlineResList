2538853250408e8da4577c7fea7f95b060e0736e
Mladen iMac <mladen0mladenovic@gmail.com>
2021-08-15 13:21:57 +0200

    Added an option for a java resource doc
    Using a keyword 'java' or 'python' you can choose which doc to update

--- onlineResList.py
+++ onlineResList.py
@@ -2,9 +2,13 @@
 
 # onlineResList - Makes an entry to the .txt doc.
 #	takes link from the clipboard, <title> from the web page and comment from the clipboard.
-#	Saves document in ~/Py directory, under the name 'online_resources.txt'
+#
+# Depending on the keyword 'java' or 'python'saves document in predefined directory, under the name 'python_resources.txt' or 'java_resources.txt'
 # Dodaje na spisak resursa za ucenje pajtona linkove sa naslovima
 # Link ucitava iz klipborda, sa sajda skida <title>, a komentar uzima sa terminala
+
+
+# Syntax: python onlineResList.py java TextOfTheNote
 
 import os, sys, pyperclip, requests, bs4
 
@@ -20,14 +24,22 @@ try:
 	title = soup.find('title').text
 except NameError: exit()
 # Checking if there are any comments added.
-if len(sys.argv) >1:
-	note =' '.join(sys.argv[1:])
+if len(sys.argv) >2:
+	note =' '.join(sys.argv[2:])
 	entry = f'{title}\n{note}\n{link}\n\n\n'
 else:
 	entry = f'{title}\n{link}\n\n\n'
 
 # Creating an entry.
-file = open('/Users/mladenimac/Py/online_resources.txt', 'a')
-file.write(entry)
-file.close()
-print('\nEntry made.\n')
+if sys.argv[1] == 'python':
+	with open('/Users/mladenimac/Py/python_resources.txt', 'a') as file:
+		file.write(entry)
+		file.close()
+	print('\nEntry made to python resouce file.\n')
+elif sys.argv[1] == 'java':
+	with open('/Users/mladenimac/Java/Java_resources.txt', 'a') as file:
+		file.write(entry)
+		file.close()
+	print('\nEntry made to java resource file.\n')
+else:
+	print('Cant determine the  document to edit./nType "java" or "python" keywords, without quotation marks.')
